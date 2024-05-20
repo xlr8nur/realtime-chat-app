c156dc79d36e3855005e7ec42a72898fb5a29276
thecodingliva <xlr8nur@protonmail.com>
2024-05-20 19:57:25 +0300

    deploymentt

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,12 +1,9 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import protectRoute from "../middleware/protectRoute.js";
+import { getUsersForSidebar } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);
-
-router.post("/login", login);
-
-router.post("/logout", logout);
+router.get("/", protectRoute, getUsersForSidebar);
 
 export default router;
